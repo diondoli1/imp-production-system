26b9bf5e5be54eace1e054d91d8b1e5458b793f5
diondoli1 <diondoli09@gmail.com>
2026-03-09 09:00:38 +0100

    Delete src directory

--- src/components/MachineStatusBadge.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export default function MachineStatusBadge({ state }) {
-  const normalized = state || "IDLE";
-  return <span className={`state-badge state-${normalized}`}>{normalized}</span>;
-}
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const MACHINE_ID = "HAAS_VF2_01";
-export const API_BASE = "http://127.0.0.1:8000";
-export const WS_URL = "ws://127.0.0.1:8000/ws";
